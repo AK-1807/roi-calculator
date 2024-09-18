6ba0f516b3340f251c4386b5d9f1be982d59fcdb
Anoop <anoop@cleardigital.com>
2024-09-18 19:04:47 +0530

    update index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { RoiCalculator } from "@/components/roi-calculator"
+// import { RoiCalculator } from "@/components/roi-calculator"
+import  BudgetChart  from "@/components/roi-calculator"
 
 export default function Page() {
-  return <><RoiCalculator /></>
+  return <><BudgetChart /></>
 }
